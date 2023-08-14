e763c5cc3ba136a4c2d8a960f4e6d06558ad85ab
Your Name <you@example.com>
2023-08-14 00:41:58 -0700

    Se hizo un dado de 6 caras

---
+++ js/06.js
@@ -0,0 +1,29 @@
+// Objetos math
+
+let resultado;
+
+resultado = Math.PI;
+resultado2 = Math.round(resultado);
+
+console.log(resultado);
+console.log(resultado2);
+
+//Redondear hacia arriba
+resultado3 = Math.ceil(resultado);
+console.log(resultado3);
+
+//Redondear hacia abajo
+resultado4 = Math.floor(resultado);
+console.log(resultado4);
+
+resultadoRaiz = Math.sqrt(144);
+console.log("Raiz cuadrada de 144: " + resultadoRaiz);
+
+resultadomin = Math.min(6,4,5,6,5,4,6,2,.1,6,5,4,6,5,1,6,5,4,3,2,1,92,6,8,6,2,1,3,2,1);
+console.log("Rresultado minimo: " + resultadomin);
+
+resultadomax = Math.max(6,4,5,6,5,4,6,2,1,6,5,4,6,5,1,6,5,4,3,2,1,92,6,8,6,2,1,3,2,1);
+console.log("Rresultado maximo: " + resultadomax);
+
+
+
---
+++ js/07.js
@@ -0,0 +1,14 @@
+// Ahora intentaré hacer un dado
+// Que caiga entre 1 o 6
+
+dado = Math.floor(Math.random() * 6)+1;
+if(dado > 6){
+    console.log("Discrepancia");
+}
+console.log(dado);
+
+
+
+
+
+
