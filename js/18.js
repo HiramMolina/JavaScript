623eece6c5abfaf85f54105b96f3a680de660ef1
Your Name <luhimoza22@hotmail.com>
2023-08-17 22:27:36 -0700

    Repaso breve de funciones - Interrumpido por lluvia

---
+++ js/18.js
@@ -0,0 +1,25 @@
+//FUNCIONES - QUE ES - COMO SE USAN Y COMO HACER LAS PROPIAS
+//DECLARACION DE FUNCION
+
+function sumar() {
+    console.log(10+10);
+}
+
+sumar();
+
+
+//EXPRESION DE LA FUNCIÓN
+
+const sumar2 = function(){
+console.log(3 + 3);
+}
+
+sumar2();
+
+// // IIFE - Funciones que no se ocupan llamar
+// // porque se llaman ellas mismas
+// (function() {
+//     console.log('Esto es una función');
+// })();
+
+console.log(navigator.plataform);
